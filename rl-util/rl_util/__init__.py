a848a55bd7e50cea5b6357357b3b84b9de4440c0
holounic <mia.ilminsckaya@yandex.ru>
2022-07-22 14:33:47 +0300

    add eps soft policy

--- rl-util/rl_util/__init__.py
+++ rl-util/rl_util/__init__.py
@@ -1,2 +1,2 @@
 from rl_util.environment import Markov, MarkovEnv
-from rl_util.policy import DeterministicPolicy, StochasticPolicy, Policy
+from rl_util.policy import DeterministicPolicy, StochasticPolicy, Policy, EpsSoftPolicy
--- rl-util/rl_util/policy.py
+++ rl-util/rl_util/policy.py
@@ -42,3 +42,39 @@ class DeterministicPolicy(Policy):
 
     def __call__(self, s):
         return self.transitions[s]
+
+
+class EpsSoftPolicy(Policy):
+    def __init__(self, state_actions: dict, eps: float, seed: int = 11):
+        self.eps = eps
+        key = random.PRNGKey(seed)
+        self.probs = {}
+
+        for state in state_actions:
+            self.probs[state] = {}
+            actions = state_actions[state]
+
+            action_probs = random.randint((key := random.split(key)[0]), (len(actions),), 1, 2 * len(actions) + 1)
+            action_probs = action_probs / action_probs.sum()
+
+            for (action, prob) in zip(actions, action_probs):
+                self.probs[state][action] = prob
+
+    def p(self, a, s):
+        return self.probs[s][a]
+
+    def __call__(self, s):
+        probs = self.probs[s]
+        max_a = probs.keys()[0]
+        for a in probs.keys():
+            if probs[a] > probs[max_a]:
+                max_a = a
+        return max_a
+
+    def update(self, s, best_a):
+        n_actions = len(self.probs[s])
+        a_star_prob = 1 - self.eps + self.eps / n_actions
+        other_prob = self.eps / n_actions
+
+        for action in self.probs[s].keys():
+            self.probs[s][action] = a_star_prob if action == best_a else other_prob
